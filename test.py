3dd28c0eb4632559860bf1dbee69f68bc04b55ae
cookie <cinuor@hotmail.com>
2015-10-24 09:52:16 +0800

    change test item

--- test.py
+++ test.py
@@ -13,17 +13,22 @@ from config.config import config
 def next_id():
 	return '%015d%s000' %(int(time.time()*1000), uuid.uuid4().hex)
 
-class User(Model):
-	id = StringField(ddl='VARCHAR(50)',primary_key=True, default=next_id())
-	name = StringField(ddl='VARCHAR(50)')
-	email = StringField(ddl='VARCHAR(50)')
+class Users(Model):
+	id = StringField(primary_key=True, default=next_id(), ddl='varchar(50)')
+	email = StringField(ddl='varchar(50)')
+	password = StringField(ddl='varchar(50)')
+	admin = BooleanField()
+	name = StringField(ddl='varchar(50)')
+	image = StringField(ddl='varchar(500)')
+	created_at = FloatField(default=time.time)
 
 #main function
 def main(loop,**kw):
 	yield from create_pools(loop = loop, pool_size=5, **kw)
-	s = yield from User.findAll()
-	x = s[0]
-	yield from x.remove()
+	s = yield from Users.findAll()
+	print (s)
+
+	
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
